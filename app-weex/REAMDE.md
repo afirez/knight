93d48f0fbb96ea8b2366f489a90e351fd4701f9d
afirez <afirez@163.com>
2018-11-28 23:51:21 +0800

    App ( Weex )  Add Weex to Andorid Project

---
+++ app-weex/REAMDE.md
@@ -0,0 +1,13 @@
+## Resource
+
+- ![Weex](https://juejin.im/post/5be2c66df265da61715dd19b)
+
+- ![Arch](https://juejin.im/post/5bea44cd6fb9a049ba411bac#heading-20)
+
+
+## Weex
+
+###
+
+- Weex 项目创建并添加 Android 平台
+- 已有 Android 项目集成 Weex
--- app-weex/src/main/java/com/afirez/app/weex/internal/adapter/ImageLoaderAdapter.kt
+++ app-weex/src/main/java/com/afirez/app/weex/internal/adapter/ImageLoaderAdapter.kt
@@ -1,6 +1,12 @@
 package com.afirez.app.weex.internal.adapter
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.taobao.weex.adapter.IWXImgLoaderAdapter
 import com.taobao.weex.common.WXImageStrategy
 import com.taobao.weex.dom.WXImageQuality
@@ -12,6 +18,33 @@ class ImageLoaderAdapter : IWXImgLoaderAdapter {
         quality: WXImageQuality?,
         strategy: WXImageStrategy?
     ) {
+        if (view != null) {
+            Glide.with(view)
+                .load(url)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        strategy?.imageListener?.onImageFinish(url, view, false, null)
+                        return false
+                    }
 
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        strategy?.imageListener?.onImageFinish(url, view, true, null)
+                        return false
+                    }
+
+                })
+                .into(view)
+        }
     }
 }
